236743f2477b1bd7278bc1878334131ff838d916
Никита Шелепов <shelepovfamily@gmail.com>
2023-12-05 18:49:48 +0500

    Added selections model

--- backend/development_tracker/selections/models.py
+++ backend/development_tracker/selections/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from skills.models import DefaultSkill
 
-# Create your models here.
+
+class Selection(models.Model):
+    name = models.CharField(max_length=100)
+    skill = models.ManyToManyField(
+        DefaultSkill,
+        through="SelectionSkill",
+        through_fields=("selection", "skill"),
+    )
+
+
+class SelectionSkill(models.Model):
+    selection = models.ForeignKey(Selection, on_delete=models.CASCADE)
+    skill = models.ForeignKey(DefaultSkill, on_delete=models.CASCADE)
