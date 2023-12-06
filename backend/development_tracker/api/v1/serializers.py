cc1f9d229bbe61912290fed1b60080d46cede288
Дмитрий Муратов <myratov91@yandex.ru>
2023-12-06 18:29:24 +0300

    added base64 and change image field in model

--- backend/development_tracker/api/v1/serializers.py
+++ backend/development_tracker/api/v1/serializers.py
@@ -1,3 +1,6 @@
+import base64
+from django.core.files.base import ContentFile
+
 from rest_framework import serializers
 
 from courses.models import Course
@@ -6,7 +9,28 @@ from users.models import UserSkill
 from selections.models import Selection
 
 
+class Base64ImageField(serializers.ImageField):
+    """
+    Преобразует base64-кодированное изображение в объект ContentFile.
+    Args:
+        data (str): base64-кодированное изображение в формате "data:image/формат;base64,данные".
+    Returns:
+        ContentFile: Объект ContentFile, представляющий декодированное изображение.
+    """
+
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+
     class Meta:
         model = Course
         fields = ("name", "image", "url")
--- backend/development_tracker/courses/admin.py
+++ backend/development_tracker/courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Course, Skill, CourseDefaultSkill
 
-# Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url',)  # Отображаемые поля в списке
+    search_fields = ('name', 'url',)  # Поля, по которым можно осуществлять поиск
+    filter_horizontal = ('skills',)  # Добавление возможности выбора навыков в горизонтальном виде
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Skill)
+admin.site.register(CourseDefaultSkill)
