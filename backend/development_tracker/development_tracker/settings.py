0d39ab1c009e47056656647e705360ab1dadcabc
Дмитрий Муратов <myratov91@yandex.ru>
2023-12-09 20:25:32 +0300

    added postgresql

--- backend/development_tracker/development_tracker/settings.py
+++ backend/development_tracker/development_tracker/settings.py
@@ -72,13 +72,22 @@ REST_FRAMEWORK = {
 
 WSGI_APPLICATION = "development_tracker.wsgi.application"
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
